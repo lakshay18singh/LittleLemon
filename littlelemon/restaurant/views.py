aa9de22687c1e0ab07ec9efa9940897bde1b66fa
Lakshay <lakshay18singh@gmail.com>
2023-06-25 22:54:26 +0530

    week2part2

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
-
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, DestroyAPIView
+from .models import Menu, Booking
+from .serializers import MenuSerializer, BookingSerializer
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 
 def index(request):
           return render(request, 'index.html', {})
+
+class MenuItemView(ListCreateAPIView):
+        queryset = Menu.objects.all()
+        serializer_class = MenuSerializer
+
+
+class SingleMenuItemView(RetrieveUpdateAPIView, DestroyAPIView):
+        queryset = Menu.objects.all()
+        serializer_class = MenuSerializer
+
+class BookingViewSet(ModelViewSet):
+        queryset = Booking.objects.all()
+        serializer_class = BookingSerializer
+        permission_classes = [IsAuthenticated]
